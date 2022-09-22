fe1ae2be555b59f6cbf45ad97baeb5c1279187c7
tsunodajapa <gabrieltsunoda@gmail.com>
2022-09-22 20:35:50 -0300

    feat: Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import "./App.css";
 import { Card } from "./components/Card";
 
-export const App  =() => {
+export const App = () => {
+  const pokemons = [
+    {
+      id: 132,
+      name: "Ditto",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    },
+    {
+      id: 132,
+      name: "Ditto",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    },
+    {
+      id: 132,
+      name: "Ditto",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    },
+    {
+      id: 132,
+      name: "Ditto",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png",
+    },
+  ];
+
   return (
     <div className="App">
       <ul>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {pokemons.map((pokemon) => (
+          <Card key={pokemon.id} {...pokemon} />
+        ))}
       </ul>
     </div>
   );
-}
+};
